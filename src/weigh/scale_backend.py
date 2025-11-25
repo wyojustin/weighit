f131b4fe9535f95d09c211fa44d9dc9adc1ac8e1
Justin Shaw <wyojustin@gmail.com>
2025-11-25 18:51:52 -0500

    Revert to Dymo scale IDs - scale now properly detected with USB power

--- src/weigh/scale_backend.py
+++ src/weigh/scale_backend.py
@@ -11,8 +11,8 @@ import hid  # from hidapi
 
 logger = logging.getLogger(__name__)
 
-VENDOR_ID = 0x1018   # Generic USB scale
-PRODUCT_ID = 0x1006  # Actual scale detected on system
+VENDOR_ID = 0x0922   # Dymo
+PRODUCT_ID = 0x8009  # S250 Digital Postal Scale
 
 
 @dataclass
