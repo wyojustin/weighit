77322f8b1e39d9db5a578363638b2f319be632c5
Justin Shaw <wyojustin@gmail.com>
2025-11-26 17:39:10 -0500

    Cleanup: Remove unused Tkinter files and fix pytest configuration

--- src/weigh/font_test_2.py
+++
@@ -1,2 +0,0 @@
-import tkinter as tk
-print(tk.Tk().tk.call("font", "metrics", "TkDefaultFont"))
--- src/weigh/launch.sh
+++
@@ -1,3 +0,0 @@
-weston --fullscreen --socket=wayland-10 &
-WAYLAND_DISPLAY=wayland-10 Xwayland :1 &
-DISPLAY=:1 python -m weigh.gui
